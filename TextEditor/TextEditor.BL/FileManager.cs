6a8397d135592d275dcd8e53255ef6cff0b7ca7a
danilfg <danilfg@yahoo.com>
2020-05-12 13:04:26 +0300

    class and library

---
+++ TextEditor/TextEditor.BL/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System.IO
+using System.Text
+
+namespace TextEditor.BL
+{
+	public class FileManager
+	{
+		private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251); // используем стандартную кодировку windows
++
+		public bool IsExist(string filePath) // проверяем наличие файла
+		{
+			bool isExist = File.Exists(filePath);
+			return isExist;
+		}
+
+		public string GetContent(string filePath) // если кодировку не указали, то используется по умолчанию 1251
+		{
+			return GetContent(filePath, _defaultEncoding);
+		}
+
+		public string GetContent(string filePath, Encoding encoding) // если указали, то используем ее.у
+		{
+			string content = File.ReadAllText(filePath, encoding); // через метод ReadAllText, класса File обрабатываем данные 
+			return content;
+		}
+
+		public void SaveContent(string content, string filePath) // сохраняем файл
+		{
+			SaveContent(content, filePath, _defaultEncoding)
+		}
+
+		public void SaveContent(string content, string filePath, Encoding encoding)
+		{
+			File.WriteAllText(filePath, content, encoding);
+		}
+
+		public int GetSymbolCount(string content) // подсчитываем количество символов
+		{
+			int count = content.Length;
+			return count;
+		}
+	}
+}
